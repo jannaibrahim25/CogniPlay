f76cd821517c6a1051effa835c16a42786a74426
coiler <coiler1@jh.edu>
2025-04-18 14:01:54 -0400

    updated scaffolding with UI parts

--- cogni_play/lib/UI/detective_game_screen.dart
+++ cogni_play/lib/UI/detective_game_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'detective_game.dart';
+import '../detective_game.dart';
 
 class DetectiveGameScreen extends StatelessWidget {
   const DetectiveGameScreen({super.key});

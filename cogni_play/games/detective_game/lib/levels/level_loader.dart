d3802a98f6a9757ef945813a33e55958f9bf8b8e
Megan Weng <mywwyx26@gmail.com>
2025-04-21 19:39:20 -0400

    comments on level loader

---
+++ cogni_play/games/detective_game/lib/levels/level_loader.dart
@@ -0,0 +1,24 @@
+import 'level_data.json';
+
+/*
+plan for this file:
+- load a new level from json file
+  - figure out how to read a json file
+  - call location based or pattern based from input
+- track the number of times a level is played (store in a variable or something)
+- user clicking input
+  - selecting places, user is a point but we will show a circle
+  - needs to be a certain radius from center of object, depends on object size
+  - submit and back and pause and stuff like that
+- something that blocks the entire screen when objects disappear
+- need to make objects disappear and know their location
+  - so we need to have object dart files
+- placing the objects and giving them locations
+  - can make a grid? or just randomize but then need to check for overlap
+  - instead of grid just have a few set spots they can be in
+    - if we do this then add set coordinates to json file
+    - in terms of scenery, this makes the most sense for object placement
+    - this way objects won't just be floating randomly
+  
+
+*/

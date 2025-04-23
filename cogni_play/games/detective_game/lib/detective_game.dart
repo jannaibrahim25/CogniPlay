929827ece1728a01cfc6583afcbe52dc536cb65e
Janna <jannaibrahim25@gmail.com>
2025-04-23 10:05:51 -0400

    uncomment import stmts for game loop

--- cogni_play/games/detective_game/lib/detective_game.dart
+++ cogni_play/games/detective_game/lib/detective_game.dart
@@ -1,10 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-//import 'components/map/map.dart';
-//import 'components/map/map.dart';
-//import 'components/characters/thief/thief.dart';
-//import 'components/objects/object_behavior.dart';
-//import 'levels/level_loader.dart';
+import 'components/map/map.dart';
+import 'components/map/map.dart';
+import 'components/characters/thief/thief.dart';
+import 'components/objects/object_behavior.dart';
+import 'levels/level_loader.dart';
 
 class DetectiveGame extends FlameGame {
   @override
